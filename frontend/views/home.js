2e9b1bbc7097b778ac1c30432c5b8eb6ebbaf074
Calvin Spealman <calvin@caktusgroup.com>
2015-01-02 14:22:35 -0500

    Added i18next to frontend deps and tested within backbone view.

--- frontend/views/home.js
+++ frontend/views/home.js
@@ -1,5 +1,6 @@
 var Backbone = require('backbone'),
-    template = require("../templates/home.hbs");
+    template = require("../templates/home.hbs"),
+    i18n = require('i18next-client');
 
 module.exports = Backbone.View.extend({
     initialize: function(){
@@ -7,6 +8,11 @@ module.exports = Backbone.View.extend({
     },
 
     render: function() {
+        var $el = this.$el;
         this.$el.html(template({name: "World"}));
+        i18n.init(function(t){
+            $el.i18n();
+            console.log(t("test.hello"));
+        })
     },
 })
