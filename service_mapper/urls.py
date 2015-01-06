bd1c283301cc0329284b222c1b0147588a2dd115
Dan Poirier <dpoirier@caktusgroup.com>
2015-01-06 14:38:50 -0500

    Add API with users and groups for something to get started with

--- service_mapper/urls.py
+++ service_mapper/urls.py
@@ -3,7 +3,10 @@ from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 
+import api.urls
+
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/', include(api.urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
