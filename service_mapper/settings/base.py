327ccb571d114a1ad7efc521ce3adcc6ac00b64c
Dan Poirier <dpoirier@caktusgroup.com>
2015-01-06 15:43:21 -0500

    Set up for translation

--- service_mapper/settings/base.py
+++ service_mapper/settings/base.py
@@ -70,6 +70,10 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
+LOCALE_PATHS = (
+    os.path.join(PROJECT_ROOT, 'locale'),
+)
+
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = (
--- service_mapper/urls.py
+++ service_mapper/urls.py
@@ -2,6 +2,13 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
+
+# FIXME: Developers, remove this once we have any real translated strings.
+# There has to be one string or none of the tools produce any output, which
+# is confusing.
+# Translators: You do NOT need to translate this:
+foo = _("THIS IS A TEST")
 
 
 urlpatterns = [
