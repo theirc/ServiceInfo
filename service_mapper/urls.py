8df53eb58f95b002f0228055fb63b4be67cd6e4e
Calvin Spealman <calvin@caktusgroup.com>
2015-01-07 15:13:20 -0500

    Setup a shell for the application that blocks out the header, footer, and layout swap between orientations.

--- service_mapper/urls.py
+++ service_mapper/urls.py
@@ -30,5 +30,5 @@ if settings.DEBUG:
         url(r'^$', RedirectView.as_view(url=settings.STATIC_URL + 'index.html'), name='index-html-redirect'),
         # The few files we want to serve statically when running locally
         url(r'^(?P<path>(index.html|bundle\.js))$', serve, kwargs={'document_root': settings.STATIC_ROOT}),
-        url(r'^(?P<path>(styles|locales)/.*)$', serve, kwargs={'document_root': settings.STATIC_ROOT}),
+        url(r'^(?P<path>(styles|images|locales)/.*)$', serve, kwargs={'document_root': settings.STATIC_ROOT}),
     ]
