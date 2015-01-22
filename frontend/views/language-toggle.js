3b92b59cbd6c0442f1b0aa7a32b2fac43b1cb540
Calvin Spealman <calvin@caktusgroup.com>
2015-01-22 09:03:39 -0500

    Animate hidding of toggle into the menu or language menu item

--- frontend/views/language-toggle.js
+++ frontend/views/language-toggle.js
@@ -26,11 +26,33 @@ module.exports = Backbone.View.extend({
         });
     },
 
+    hide: function() {
+        var target;
+        if (window.innerHeight > window.innerWidth){
+            target = $('#menu-toggle').position();
+        } else {
+            target = $('#menu-container').find('.menu-item-language').position();
+        }
+        var anim = {
+            top: target.top,
+            left: target.left,
+        };
+        this.$el.addClass('hidden');
+        this.$el.animate(anim, {duration: 0.5});
+    },
+
+    show: function() {
+        this.$el.css('top', 'auto');
+        this.$el.css('left', 'auto');
+        this.$el.css.removeClass('hidden');
+    },
+
     events: {
         "click button": function(ev) {
             var lang = $(ev.target).data('lang');
             localStorage['lang'] = lang;
             this.setLanguage(lang);
+            this.hide();
         },
     },
 })
