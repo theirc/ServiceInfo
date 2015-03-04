7e606e01275dfbfedecade6b1fcd890119f6a4b0
Calvin Spealman <calvin@caktusgroup.com>
2015-03-04 14:35:10 -0500

    Functional map view of public services with associated locations

--- frontend/views/map.js
+++ frontend/views/map.js
@@ -1,14 +1,54 @@
 var Backbone = require('backbone'),
     template = require("../templates/map.hbs"),
+    models = require('../models/service'),
     i18n = require('i18next-client');
 
 module.exports = Backbone.View.extend({
     initialize: function(){
-        this.render();
+        var self = this;
+        this.services = new models.PublicServices();
+        this.services.fetch().then(function(){
+            self.render();
+        })
     },
 
     render: function() {
         var $el = this.$el;
-        this.$el.html(template({}));
+        var map;
+        this.$el.html(template({
+            services: this.services,
+        }));
+
+        var services = this.services.data();
+        function initialize() {
+            var mapOptions = {
+                center: { lat: -34.397, lng: 150.644},
+                zoom: 8
+            };
+            map = new google.maps.Map(document.getElementById('map_canvas'),
+                mapOptions);
+            var bounds = new google.maps.LatLngBounds();
+
+            $.each(services, function() {
+                var service = this;
+                var latlng_string = /(\d+\.\d+) (\d+\.\d+)/.exec(this.location);
+                if (latlng_string) {
+                    var myLatlng = new google.maps.LatLng(latlng_string[1], latlng_string[2]);
+                    bounds.extend(myLatlng);
+                    var marker = new google.maps.Marker({
+                        position: myLatlng,
+                        title: this.name,
+                    });
+                    marker.setMap(map);
+                    google.maps.event.addListener(marker, 'click', function() {
+                        location.hash = '#/services/' + service.id + '/';
+                    })
+                }
+            });
+
+            map.fitBounds(bounds);
+        }
+
+        initialize();
     },
 })
