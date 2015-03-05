4942c28dd412f2e11cb9f56797313538b875bf8b
Calvin Spealman <calvin@caktusgroup.com>
2015-03-05 14:46:12 -0500

    Connect a search query input to fetch new results on demand

--- frontend/views/map.js
+++ frontend/views/map.js
@@ -23,51 +23,63 @@ module.exports = Backbone.View.extend({
 
     render: function() {
         var $el = this.$el;
-        var map;
+        var self=this;
         this.$el.html(template({
             services: this.services,
         }));
 
-        var services = this.services.data();
+
         function initialize() {
             var mapOptions = {
                 center: { lat: -34.397, lng: 150.644},
                 zoom: 8
             };
-            map = new google.maps.Map(document.getElementById('map_canvas'),
+            self.map = new google.maps.Map(document.getElementById('map_canvas'),
                 mapOptions);
-            var bounds = new google.maps.LatLngBounds();
-
-            $.each(services, function() {
-                var service = this;
-                var latlng_string = /(\d+\.\d+) (\d+\.\d+)/.exec(this.location);
-                if (latlng_string) {
-                    var myLatlng = new google.maps.LatLng(latlng_string[1], latlng_string[2]);
-                    bounds.extend(myLatlng);
-                    var marker = new google.maps.Marker({
-                        position: myLatlng,
-                        title: this.name,
-                    });
-                    marker.setMap(map);
-                    google.maps.event.addListener(marker, 'click', function() {
-                        location.hash = '#/services/' + service.id + '/';
-                    })
-                }
-            });
-
-            map.fitBounds(bounds);
+            self.updateMarkers();
         }
 
         initialize();
+    },
+
+    updateMarkers: function() {
+        var self = this;
+        var bounds = new google.maps.LatLngBounds();
+        var services = this.services.data();
+        $.each(services, function() {
+            var service = this;
+            var latlng_string = /(\d+\.\d+) (\d+\.\d+)/.exec(this.location);
+            if (latlng_string) {
+                var myLatlng = new google.maps.LatLng(latlng_string[1], latlng_string[2]);
+                bounds.extend(myLatlng);
+                var marker = new google.maps.Marker({
+                    position: myLatlng,
+                    title: this.name,
+                });
+                marker.setMap(self.map);
+                google.maps.event.addListener(marker, 'click', function() {
+                    location.hash = '#/services/' + service.id + '/';
+                })
+            }
+        });
+        self.map.fitBounds(bounds);
     },
 
     events: {
         "search": function(_, query) {
             this.query = query;
             var self = this;
-            this.services.fetch({data: {name: query}}).then(function(){
-                self.render();
+            this.services.fetch({data: {name: self.query}}).then(function(){
+                self.updateMarkers();
             })
-        }
+        },
+        "input input.query": function(e) {
+            var v = $(e.target).val();
+            this.query = v;
+            var self = this;
+            this.services.fetch({data: {name: self.query}}).then(function(){
+                self.updateMarkers();
+            })
+        },
     }
 })
