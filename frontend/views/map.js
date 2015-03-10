64fab88d208f99003a768cb9269547262b8a001c
Calvin Spealman <calvin@caktusgroup.com>
2015-03-10 06:51:03 -0400

    Fix map service link

--- frontend/views/map.js
+++ frontend/views/map.js
@@ -62,7 +62,7 @@ module.exports = Backbone.View.extend({
                 });
                 marker.setMap(self.map);
                 google.maps.event.addListener(marker, 'click', function() {
-                    location.hash = '#/service/' + service.id + '/';
+                    location.hash = '#/service/' + service.id;
                 })
             }
         });
