c41bec6c73fbea713c9e707a837768cf4aa48a24
Calvin Spealman <calvin@caktusgroup.com>
2015-03-16 10:17:30 -0400

    In the provider service list, show a link to form if you can edit the service, otherwise to the detail page; Remove debugging log calls; Fix a bug that broke displaying service details without a location;

--- frontend/views/service-detail.js
+++ frontend/views/service-detail.js
@@ -28,19 +28,20 @@ module.exports = Backbone.View.extend({
         /*  https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=600x300&maptype=roadmap&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318 &markers=color:red%7Clabel:C%7C40.718217,-73.998284
         */
 
-        var url = "https://maps.googleapis.com/maps/api/staticmap?";
-        var long_lat_str = /(-?\d+\.\d+) (-?\d+\.\d+)/.exec(location);
-        url = url + "center=" + long_lat_str[2] + "," + long_lat_str[1];
-        url = url + "&zoom=8";
-        url = url + "&size=640x150";
-        url = url + "&markers=color:red%7C" + long_lat_str[2] + "," + long_lat_str[1];
+        if (location) {
+            var url = "https://maps.googleapis.com/maps/api/staticmap?";
+            var long_lat_str = /(-?\d+\.\d+) (-?\d+\.\d+)/.exec(location);
+            url = url + "center=" + long_lat_str[2] + "," + long_lat_str[1];
+            url = url + "&zoom=8";
+            url = url + "&size=640x150";
+            url = url + "&markers=color:red%7C" + long_lat_str[2] + "," + long_lat_str[1];
 
-        return url;
+            return url;
+        }
     },
 
     render: function() {
         var $el = this.$el;
-        console.log(this.service);
         $el.html(template({
             service: this.service,
             mapURL: this.getStaticMap(this.service.location),
