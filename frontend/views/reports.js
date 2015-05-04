c3ddf421d5a2f113430e9558b2a6076a897e075f
Mark Lavin <mlavin@caktusgroup.com>
2015-05-04 14:48:09 -0400

    Working on form to select reports from available reports.

--- frontend/views/reports.js
+++ frontend/views/reports.js
@@ -1,10 +1,33 @@
 var Backbone = require('backbone'),
-    template = require("../templates/reports.hbs")
+    _ = require('underscore'),
+    template = require("../templates/reports.hbs"),
+    hashtrack = require('hashtrack')
 ;
 
 
 module.exports = Backbone.View.extend({
+    events: {
+        'change :input[name="stat"]': 'updateReport'
+    },
+
+    initialize: function () {
+        this.report = hashtrack.getVar('stat') || 'wait-times';
+        this.reportOptions = [
+            {value: 'wait-times', label: 'WaitTime'}
+        ];
+    },
+
     render: function() {
-        this.$el.html(template({}));
+        var context = {
+            options: _.each(this.reportOptions, function (option) {
+                option.selected = option.value == this.report;
+                return option;
+            }, this)
+        };
+        this.$el.html(template(context));
+    },
+
+    updateReport: function (e) {
+        hashtrack.setVar('stat', e.target.value);
     }
-});
+}); 
