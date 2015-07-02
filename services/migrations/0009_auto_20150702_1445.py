fd1f5b8818416a05158f086b74519b66f599f723
Dan Poirier <dpoirier@caktusgroup.com>
2015-07-02 08:03:45 -0400

    22 Don't require staff sat in feedback if service not delivered
    Fixes #22

---
+++ services/migrations/0009_auto_20150702_1445.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.core.validators
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('services', '0008_requestforservice'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='feedback',
+            name='staff_satisfaction',
+            field=models.SmallIntegerField(validators=[django.core.validators.MinValueValidator(1), django.core.validators.MaxValueValidator(5)], help_text='How would you rate your satisfaction with the staff of the organization that provided services to you, (from 1 to 5, where 5 is the highest rating possible)?', blank=True, null=True, default=None),
+            preserve_default=True,
+        ),
+    ]
