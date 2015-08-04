485221a6fa9cae05f58e2056a7867ea6fa21d495
Vinod Kurup <vinod@kurup.com>
2015-08-04 14:17:27 -0400

    Check for migrations during test runs

--- run_tests.sh
+++ run_tests.sh
@@ -6,3 +6,4 @@ flake8 .
 rm -f .coverage
 coverage run manage.py test --noinput --settings=service_info.settings.dev "$@"
 coverage report
+python manage.py makemigrations --dry-run | grep 'No changes detected' || (echo 'There are changes which require migrations.' && exit 1)
