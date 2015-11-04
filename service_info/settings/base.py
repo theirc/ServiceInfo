c930cce846f33c0845c3f4b900d5ceefa2269910
Neil Ashton <neil.m.ashton@gmail.com>
2015-11-04 12:16:34 -0500

    RTL language support and much much more
    Various new UI features, the most important of which is added RTL support.

--- service_info/settings/base.py
+++ service_info/settings/base.py
@@ -408,6 +408,7 @@ CMS_TEMPLATES = (
     ('cms/content-types/page.html', 'Page'),
     ('cms/content-types/pages-index.html', 'Content Index'),
     ('cms/content-types/faq.html', 'FAQ'),
+    ('cms/content-types/full-width-image.html', 'Full Width Image'),
 )
 
 CMS_PERMISSION = True
