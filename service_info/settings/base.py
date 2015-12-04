9ac081ec91a1697eb1b0f98622e25e324e645c8f
Jeff Trawick <trawick@gmail.com>
2015-12-04 09:08:59 -0500

    Switch to new djangocms-column release
    That resolves the MIGRATION_MODULES mess with Django >= 1.7.

--- service_info/settings/base.py
+++ service_info/settings/base.py
@@ -433,7 +433,6 @@ MIGRATION_MODULES = {
     'cmsplugin_filer_image': 'cmsplugin_filer_image.migrations_django',
     'cmsplugin_filer_teaser': 'cmsplugin_filer_teaser.migrations_django',
     'cmsplugin_filer_video': 'cmsplugin_filer_video.migrations_django',
-    'djangocms_column': 'djangocms_column.migrations_django',
 }
 
 THUMBNAIL_PROCESSORS = (
