c3eba2b48cbb3dbfc4f1af9e127216b1196cfaf5
Neil Ashton <neil.m.ashton@gmail.com>
2015-12-09 15:36:21 -0500

    Only one dropdown menu open at a time

--- service_info/static/js/src/component/menu.js
+++ service_info/static/js/src/component/menu.js
@@ -7,13 +7,27 @@ function init () {
     });
 
     $(menu.parent_items).click(function (e) {
+        var $this = $(this);
+        var $others;
         e.preventDefault();
         console.log(e);
-        $(this).toggleClass('active');
-        $(this).parent().toggleClass('active');
-        $(this).next('ul').slideToggle({
+        $this.parent().toggleClass('active');
+        $this.next('ul').slideToggle({
             duration: 200
         });
+
+        /*
+            Close drop-down menus that aren't this one
+            or the parent of this one.
+        */
+        $others = $('#menu > li.parent')
+            .not($this.closest('#menu > li.parent'));
+        $others.removeClass('active');
+        $others.find('ul').first()
+            .slideUp({
+                duration: 200
+            })
+        ;
     });
 }
 
