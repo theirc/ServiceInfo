2164a8dffefbb4cc7d1c9ae87bb2b1ca5ae17631
Neil Ashton <neil.m.ashton@gmail.com>
2015-12-10 10:53:27 -0500

    Basic drop-down menus

--- service_info/static/js/src/index.js
+++ service_info/static/js/src/index.js
@@ -3,4 +3,11 @@ jQuery(function ($) {
     Activate Materialize mobile menu.
   */
   $(".button-collapse").sideNav();
+
+  /*
+    Activate Materialize dropdowns.
+  */
+  $(".dropdown-button").each(function () {
+    $(this).dropdown();
+  });
 });
