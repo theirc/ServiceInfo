0c35342e9d49da8aa9da17bfd53bd00eaad1088a
Neil Ashton <neil.m.ashton@gmail.com>
2015-12-16 11:44:56 -0500

    Using NaN in preference to -1

--- service_info/static/js/src/index.js
+++ service_info/static/js/src/index.js
@@ -1,20 +1,21 @@
 function getInternetExplorerVersion () {
 //http://stackoverflow.com/questions/17907445/how-to-detect-ie11
-  var rv = -1;
+  var rv = NaN;
+  
   if (navigator.appName === 'Microsoft Internet Explorer') {
     var ua = navigator.userAgent;
     var re = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
     if (re.exec(ua) != null) {
       rv = parseFloat( RegExp.$1 );
     }
-  }
-  else if (navigator.appName === 'Netscape') {
+  } else if (navigator.appName === 'Netscape') {
     var ua = navigator.userAgent;
     var re = new RegExp("Trident/.*rv:([0-9]{1,}[\.0-9]{0,})");
     if (re.exec(ua) != null) {
       rv = parseFloat( RegExp.$1 );
     }
   }
+
   return rv;
 }
 
@@ -46,7 +47,7 @@ jQuery(function ($) {
   /*
     Adjust for IE 11.
   */
-  if (getInternetExplorerVersion() !== -1) {
+  if (!isNaN(getInternetExplorerVersion())) {
     $('body').addClass('InternetExplorer');
   }
 });
