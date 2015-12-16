000f7dafc2da8d927ea1e90822eb8cbe4dda4193
Neil Ashton <neil.m.ashton@gmail.com>
2015-12-16 09:23:20 -0500

    Compensation for IE flexbox quirks

--- service_info/static/js/src/index.js
+++ service_info/static/js/src/index.js
@@ -1,3 +1,23 @@
+function getInternetExplorerVersion () {
+//http://stackoverflow.com/questions/17907445/how-to-detect-ie11
+  var rv = -1;
+  if (navigator.appName === 'Microsoft Internet Explorer') {
+    var ua = navigator.userAgent;
+    var re = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+    if (re.exec(ua) != null) {
+      rv = parseFloat( RegExp.$1 );
+    }
+  }
+  else if (navigator.appName === 'Netscape') {
+    var ua = navigator.userAgent;
+    var re = new RegExp("Trident/.*rv:([0-9]{1,}[\.0-9]{0,})");
+    if (re.exec(ua) != null) {
+      rv = parseFloat( RegExp.$1 );
+    }
+  }
+  return rv;
+}
+
 jQuery(function ($) {
   /*
     Activate Materialize mobile menu.
@@ -21,5 +41,12 @@ jQuery(function ($) {
   */
   if (!$('.cms-toolbar-expanded').length) {
     $('.parallax').parallax();
+  }
+
+  /*
+    Adjust for IE 11.
+  */
+  if (getInternetExplorerVersion() !== -1) {
+    $('body').addClass('InternetExplorer');
   }
 });
