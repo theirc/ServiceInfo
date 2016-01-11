7badc31c1a7c87269ef63b72c87ff9845a1d10b0
Rob Lineberger <rlineberger@caktusgroup.com>
2016-01-11 12:02:24 -0500

    draft of extension code

--- service_info_cms/models.py
+++ service_info_cms/models.py
@@ -5,12 +5,6 @@ from cms.extensions import PageExtension
 from cms.extensions.extension_pool import extension_pool
 
 ICON_CATALOG = 'http://materializecss.com/icons.html'
-RATING_CHOICES = [(1, '1 star'),
-                  (2, '2 stars'),
-                  (3, '3 stars'),
-                  (4, '4 stars'),
-                  (5, '5 stars')
-                  ]
 
 
 class IconNameExtension(PageExtension):
@@ -22,14 +16,18 @@ extension_pool.register(IconNameExtension)
 
 
 class RatingExtension(PageExtension):
-    rating = models.IntegerField()
-    # user
-    # user_rating
+    include_rating = models.BooleanField(default=False)
+
+extension_pool.register(RatingExtension)
+
+RATING_CHOICES = [(1, '1 star'),
+                  (2, '2 stars'),
+                  (3, '3 stars'),
+                  (4, '4 stars'),
+                  (5, '5 stars')
+                  ]
 
     def get_star_average(self):
         """find the average number of stars for this
            page and round to the closest integer"""
         return 3  # <<< TODO: calculate average
-
-extension_pool.register(IconNameExtension)
-extension_pool.register(RatingExtension)
