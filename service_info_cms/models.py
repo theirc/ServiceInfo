bb285b89f1cf82f3b4c01d5b8497942a50379106
Rob Lineberger <rlineberger@caktusgroup.com>
2016-01-11 14:56:55 -0500

    adding model for PageRatings

--- service_info_cms/models.py
+++ service_info_cms/models.py
@@ -5,7 +5,12 @@ from cms.extensions import PageExtension
 from cms.extensions.extension_pool import extension_pool
 
 ICON_CATALOG = 'http://materializecss.com/icons.html'
-
+RATING_CHOICES = [(1, '1 star'),
+                  (2, '2 stars'),
+                  (3, '3 stars'),
+                  (4, '4 stars'),
+                  (5, '5 stars')
+                  ]
 
 class IconNameExtension(PageExtension):
     icon_name = models.CharField(
@@ -20,14 +25,14 @@ class RatingExtension(PageExtension):
 
 extension_pool.register(RatingExtension)
 
-RATING_CHOICES = [(1, '1 star'),
-                  (2, '2 stars'),
-                  (3, '3 stars'),
-                  (4, '4 stars'),
-                  (5, '5 stars')
-                  ]
 
-    def get_star_average(self):
+class PageRating(PageExtension):
+    average_rating = models.DecimalField(default=False)
+    num_ratings = IntegerField()
+    rating_total = IntegerField()
+    page_id = IntegerField()
+
+    def update_rating_average(self):
         """find the average number of stars for this
            page and round to the closest integer"""
         return 3  # <<< TODO: calculate average
