c9f4d472a6ec7fc0454e87aac64c120062adae6a
Rob Lineberger <rlineberger@caktusgroup.com>
2016-01-14 12:49:43 -0500

    addressed all feedback.

--- service_info_cms/admin.py
+++ service_info_cms/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from cms.extensions import PageExtensionAdmin
 
-from .models import IconNameExtension, RatingExtension
+from .models import IconNameExtension, RatingExtension, PageRating
 
 
 class IconNameExtensionAdmin(PageExtensionAdmin):
@@ -12,5 +12,14 @@ class RatingExtensionAdmin(PageExtensionAdmin):
     pass
 
 
+class PageRatingAdmin(admin.ModelAdmin):
+    list_display = ('page_obj', 'num_ratings', 'average_rating',)
+    readonly_fields = ('page_obj', 'num_ratings', 'average_rating', )
+    list_filter = ('average_rating', )
+    search_fields = ()
+    ordering = ('page_obj', 'num_ratings', 'average_rating', '-num_ratings', '-average_rating', )
+
+
 admin.site.register(IconNameExtension, IconNameExtensionAdmin)
 admin.site.register(RatingExtension, RatingExtensionAdmin)
+admin.site.register(PageRating, PageRatingAdmin)
