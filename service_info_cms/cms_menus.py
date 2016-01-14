0a3662adf4502d3d53f556889751be93c52f8ece
Rob Lineberger <rlineberger@caktusgroup.com>
2016-01-14 10:00:13 -0500

    PR feedback part 1

--- service_info_cms/cms_menus.py
+++ service_info_cms/cms_menus.py
@@ -26,21 +26,21 @@ class AddIconNameExtension(Modifier):
         return nodes
 
 
-class AddRatingExtension(Modifier):
-    """Make the RatingExtension available in the menu."""
+# class AddRatingExtension(Modifier):
+#     """Make the RatingExtension available in the menu."""
 
-    def _modify_nodes(self, nodes):
-        for node in nodes:
-            if node.namespace == CMS_PAGE_NODE_NAMESPACE:
-                page = Page.objects.get(pk=node.id)
-                if getattr(page, 'ratingextension', None):
-                    setattr(node, 'iratingextension', page.ratingextension)
-                self._modify_nodes(node.children)
+#     def _modify_nodes(self, nodes):
+#         for node in nodes:
+#             if node.namespace == CMS_PAGE_NODE_NAMESPACE:
+#                 page = Page.objects.get(pk=node.id)
+#                 if getattr(page, 'ratingextension', None):
+#                     setattr(node, 'iratingextension', page.ratingextension)
+#                 self._modify_nodes(node.children)
 
-    def modify(self, request, nodes, namespace, root_id, post_cut, breadcrumb):
-        if post_cut and not breadcrumb:
-            self._modify_nodes(nodes)
-        return nodes
+#     def modify(self, request, nodes, namespace, root_id, post_cut, breadcrumb):
+#         if post_cut and not breadcrumb:
+#             self._modify_nodes(nodes)
+#         return nodes
 
 
 menu_pool.register_modifier(AddIconNameExtension)
