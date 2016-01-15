c0ce6c6f351d903bcd4aba13baf68cbc64f856d8
Jeff Trawick <trawick@gmail.com>
2016-01-15 18:21:53 -0500

    Separate the "testcase" part of checking migrations vs. I/O and parsing

--- service_info/tests/test_missing_migrations.py
+++ service_info/tests/test_missing_migrations.py
@@ -7,7 +7,7 @@ from django.core import management
 
 EXPECTED_CHANGES = {
     'aldryn_faq': [
-        # Py3K-izing default=b'{}', which is interpreted with encoding UTF-8
+        # Py3K-izing default=b'{}', which is interpreted with encoding UTF-8:
         # migrations.AlterField(
         #     model_name='faqconfig',
         #     name='app_data',
@@ -17,31 +17,48 @@ EXPECTED_CHANGES = {
 }
 
 
+class MigrationCommandOutput(object):
+    """Parse output from makemigrations command, generating a series of
+      (module, changed_fields)"""
+
+    INITIAL = 'INITIAL'
+    READING = 'READING'  # reading changes for package
+
+    def __init__(self, input_file):
+        self.input = input_file
+
+    def read(self):
+        actual_changes = []
+        module = ''
+        state = self.INITIAL
+        while True:
+            l = self.input.readline()
+            if l in ('', 'No changes detected'):
+                break
+            if l.startswith('Migrations'):
+                if state != self.INITIAL:
+                    yield module, actual_changes
+                state = self.READING
+                module = re.match(r'^Migrations for \'(.+)\'', l).group(1)
+            elif re.search(r'^ +.*\.py:', l):
+                continue
+            else:
+                if state != self.READING:
+                    raise ValueError('State is <%s> instead of <R>' % state)
+                actual_changes.append(l.strip())
+        if state != self.INITIAL:
+            yield module, actual_changes
+
+
 class MissingMigrationTestCase(TestCase):
 
     def test(self):
-        state = 'I'  # 'I' == initial, 'R' == reading changes for module
-        actual_changes = []
-        module = ''
         actual_modules = []
         with io.StringIO() as f:
             management.call_command('makemigrations', dry_run=True, stdout=f, no_color=True)
             f.seek(0)
-            while True:
-                l = f.readline()
-                if l in ('', 'No changes detected'):
-                    break
-                if l.startswith('Migrations'):
-                    if state != 'I':
-                        self.assertEqual(EXPECTED_CHANGES[module], actual_changes)
-                    state = 'R'
-                    module = re.match(r'^Migrations for \'(.+)\'', l).group(1)
-                    actual_modules.append(module)
-                elif re.search(r'^ +.*\.py:', l):
-                    continue
-                else:
-                    self.assertTrue(state == 'R')
-                    actual_changes.append(l.strip())
-        if state != 'I':
-            self.assertEqual(EXPECTED_CHANGES[module], actual_changes)
+            migrations = MigrationCommandOutput(f)
+            for module, actual_changes in migrations.read():
+                actual_modules.append(module)
+                self.assertEqual(EXPECTED_CHANGES[module], actual_changes)
         self.assertEqual(sorted(EXPECTED_CHANGES.keys()), sorted(actual_modules))
